75e2fb3ddb0f092717daa59cab76dd4a6b4c4a77
Andrew Smith <andrew@andrew.codes>
2017-06-19 08:36:47 -0400

    Enable client hot reloading when in development

--- packages/server-app/src/webServer/app.js
+++ packages/server-app/src/webServer/app.js
@@ -1,9 +1,10 @@
 import {HelloWorld} from '@rdhariwal/fpm-web';
 import {renderToString} from 'react-dom/server';
+import renderHtml from './renderHtml';
 
 export default (server) => {
-    server.get('*', (req, res) => {
-       res.status(200).send(renderToString(HelloWorld()));
+    server.get('/', (req, res) => {
+       res.status(200).send(renderHtml({body: renderToString(HelloWorld())}));
     });
     return server;
 }
